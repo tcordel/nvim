6e67304fde5764c62c4d405857d6478f6b083962
Thibault Cordel <tcordel@enovacom.fr>
2024-03-22 14:51:54 +0100

    feat: java and ts are working

---
+++ init.lua
@@ -0,0 +1,18 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+require("lspconfig").jdtls.setup({
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = "/home/share/jdk-21.0.1+12",
+            default = true,
+          },
+        },
+      },
+    },
+  },
+})
