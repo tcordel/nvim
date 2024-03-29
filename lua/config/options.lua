18a6796ab2cd5de5ca249ffbed23d6dd0a811d08
Thibault Cordel <tcordel@enovacom.fr>
2024-03-29 16:46:33 +0100

    fix: tab is now 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,6 +12,6 @@ vim.opt.showcmd = true
 vim.opt.formatoptions:append({ "r" })
 
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 --vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
