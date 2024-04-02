accd7f90db6ca7cfecfaf09c47ffc1b19a4b6d3f
Thibault Cordel <tcordel@enovacom.fr>
2024-04-02 12:09:06 +0200

    feat: add harpoon

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -64,7 +64,35 @@ return {
       },
     }
 
+    dap.configurations.javascript= { -- change this to javascript if needed
+      {
+
+        name = "Debug with Chrome",
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+      },
+    }
     dap.configurations.typescriptreact = { -- change to typescript if needed
+      {
+        name = "Debug with Chrome",
+        type = "chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = "inspector",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+      },
+    }
+
+    dap.configurations.typescript = { -- change to typescript if needed
       {
         name = "Debug with Chrome",
         type = "chrome",
