973f0c5d959e32815bff980293b1d9fdd891457c
Thibault Cordel <tcordel@enovacom.fr>
2024-04-10 17:52:36 +0200

    fix: make jest work

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,53 +1,42 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-neotest/neotest-jest",
-    },
-    requires = {
-      "nvim-neotest/neotest-jest",
-    },
-    keys = {
-      {
-        "<leader>tL",
-        function()
-          require("neotest").run.run_last({ strategy = "dap" })
-        end,
-        desc = "Debug Last Test",
-      },
-      {
-        "<leader>ts",
-        function()
-          require("neotest").summary.toggle()
-        end,
-        desc = "Show/hide tests panel",
-      },
-      {
-        "<leader>to",
-        function()
-          require("neotest").output.open()
-        end,
-        desc = "Show output",
-      },
-    },
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("neotest").setup({
-        adapters = {
-          require("neotest-jest")({
-            jestCommand = "yarn test --",
-            jestConfigFile = "custom.jest.config.ts",
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          }),
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/neotest-jest",
         },
-      })
-    end,
-  },
+        requires = {
+            "nvim-neotest/neotest-jest",
+        },
+        keys = {
+            { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end,                      desc = "Run File" },
+            { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end,                            desc = "Run All Test Files" },
+            { "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end,                    desc = "Debug Nearest" },
+            { "<leader>tr", function() require("neotest").run.run() end,                                        desc = "Run Nearest" },
+            { "<leader>tl", function() require("neotest").run.run_last() end,                                   desc = "Run Last" },
+            { "<leader>ts", function() require("neotest").summary.toggle() end,                                 desc = "Toggle Summary" },
+            { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
+            { "<leader>tO", function() require("neotest").output_panel.toggle() end,                            desc = "Toggle Output Panel" },
+            { "<leader>tS", function() require("neotest").run.stop() end,                                       desc = "Stop" },
+
+        },
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-jest")({
+                        jestCommand = "yarn test --",
+                        jestConfigFile = "custom.jest.config.ts",
+                        env = { CI = true },
+                        cwd = function(path)
+                            return vim.fn.getcwd()
+                        end,
+                    }),
+                },
+            })
+        end,
+    },
 }
