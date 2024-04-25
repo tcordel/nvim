34e83b35e6580efb3289c150999a692993c65115
Thibault Cordel <tcordel@enovacom.fr>
2024-04-25 15:38:06 +0200

    feat: dap keys

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,6 +10,10 @@ return {
             end,
         },
     },
+	keys = {
+		{ "<leader>dO", function() require("dap").step_out() end,  desc = "Step Out" },
+		{ "<leader>do", function() require("dap").step_over() end, desc = "Step Over" },
+	},
     opts = function()
         local dap = require("dap")
         dap.configurations.java = {
