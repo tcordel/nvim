b6aa76c9222f2686d400ce1e097864cc614ba5d0
Thibault Cordel <tcordel@enovacom.fr>
2024-08-27 15:36:51 +0200

    fix: disable root dir for jdtls

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -38,27 +38,27 @@ return {
     },
 
     opts = function(_, opts)
-      opts.root_dir = function(bufname)
-        local path = require("jdtls.path")
-        local dirname = vim.fn.fnamemodify(bufname, ":p:h")
-        local getparent = function(p)
-          return vim.fn.fnamemodify(p, ":h")
-        end
-        local pom = nil
-        while getparent(dirname) ~= dirname do
-          if vim.loop.fs_stat(path.join(dirname, "JenkinsFile")) then
-            return dirname
-          end
-
-          if vim.loop.fs_stat(path.join(dirname, "pom.xml")) then
-            pom = dirname
-          elseif pom ~= nil then
-            return pom
-          end
-          dirname = getparent(dirname)
-        end
-        return dirname
-      end
+      -- opts.root_dir = function(bufname)
+      --   local path = require("jdtls.path")
+      --   local dirname = vim.fn.fnamemodify(bufname, ":p:h")
+      --   local getparent = function(p)
+      --     return vim.fn.fnamemodify(p, ":h")
+      --   end
+      --   local pom = nil
+      --   while getparent(dirname) ~= dirname do
+      --     if vim.loop.fs_stat(path.join(dirname, "JenkinsFile")) then
+      --       return dirname
+      --     end
+      --
+      --     if vim.loop.fs_stat(path.join(dirname, "pom.xml")) then
+      --       pom = dirname
+      --     elseif pom ~= nil then
+      --       return pom
+      --     end
+      --     dirname = getparent(dirname)
+      --   end
+      --   return dirname
+      -- end
       opts.cmd = {
         vim.fn.exepath("jdtls"),
         "--jvm-arg=-javaagent:" .. require("mason-registry").get_package("jdtls"):get_install_path() .. "/lombok.jar",
