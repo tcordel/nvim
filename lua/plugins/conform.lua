02536237590a314dbc96a64d421663d2e11199a7
Thibault Cordel <tcordel@enovacom.fr>
2024-09-02 08:21:59 +0200

    feat(conform): xml

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,32 +1,41 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		lang_to_formatters = {
-			json = { "jq" },
-			xml = { "xmlfomat" },
-		},
-		formatters_by_ft = {
-			lua = { "stylua" },
-			markdown = { "prettier" },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			xml = { "xmlfomatter" },
-		},
-	},
-	init = function()
-		vim.o.formatexpr = "v:lua.require'conform'.format({ async = true, lsp_fallback = true })"
-	end,
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>cf",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	}
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      markdown = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      xml = { "tidy" },
+    },
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      tidy = {
+        command = "tidy",
+        args = {
+          "-q",
+          "--input-xml",
+          "true",
+          "--indent",
+          "yes",
+          "--indent-spaces",
+          "4",
+        },
+        stdin = true,
+      },
+    },
+  },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
 }
