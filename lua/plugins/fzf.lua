aaa6238fbdb571a97c71a341eddf73642284d190
Thibault Cordel <tcordel@enovacom.fr>
2024-12-20 10:42:25 +0100

    feat: fzf code actions

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,10 +1,18 @@
 return {
 	"ibhagwan/fzf-lua",
 	opts = function(_, opts)
+		local file_win_opts = {
+			fullscreen = true,
+			preview = {
+				layout = "vertical",
+				vertical = "up:70%",
+			},
+		}
 		return {
 			fzf_opts = {
 				["--no-scrollbar"] = false,
 			},
+			winopts = file_win_opts,
 		}
 	end,
 	keys = {
@@ -14,6 +22,14 @@ return {
 				require("fzf-lua").lsp_incoming_calls()
 			end,
 			desc = "lsp_incoming_calls",
+		},
+
+		{
+			"<leader>ca",
+			function()
+				require("fzf-lua").lsp_code_actions()
+			end,
+			desc = "lsp_code_actions",
 		},
 		{
 			"<leader>fs",
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,12 @@
 return {
 	"williamboman/mason.nvim",
 	opts = {
-		ensure_installed = { "xmlformatter", "powershell-editor-services", "bash-language-server" },
+		ensure_installed = {
+			"xmlformatter",
+			"powershell-editor-services",
+			"bash-language-server",
+			"lua-language-server"
+		},
 		registries = {
 			"github:mason-org/mason-registry",
 			"github:nvim-java/mason-registry",
