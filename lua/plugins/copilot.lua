90c676efec348acf20bfbded284e372fb3deecf7
Thibault Cordel <tcordel@enovacom.fr>
2025-04-11 15:44:03 +0200

    feat: copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+	"zbirenbaum/copilot.lua",
+	opts = {
+		copilot_node_command = "/home/tib/.nvm/versions/node/v22.14.0/bin/node",
+		filetypes = {
+			markdown = true,
+			lua = true,
+			javascript = true,
+			bash = true,
+			typescript = true,
+			java=true,
+			dockerfile=true,
+			["*"] = false,
+		},
+
+	}
+}
