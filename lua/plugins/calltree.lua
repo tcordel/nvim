d763cc18c6fd9c689871a525d6f463f5f157cfdb
Thibault Cordel <tcordel@enovacom.fr>
2025-11-09 13:29:17 +0100

    feat: calltree

---
+++ lua/plugins/calltree.lua
@@ -0,0 +1,53 @@
+return {
+	{
+		"ldelossa/litee.nvim",
+		event = "VeryLazy",
+		opts = {
+			notify = { enabled = false },
+			panel = {
+				orientation = "bottom",
+				panel_size = 10,
+			},
+		},
+		config = function(_, opts)
+			require("litee.lib").setup(opts)
+		end,
+	},
+
+	{
+		"ldelossa/litee-calltree.nvim",
+		dependencies = "ldelossa/litee.nvim",
+		event = "VeryLazy",
+		opts = {
+			on_open = "panel",
+			map_resize_keys = false,
+		},
+		config = function(_, opts)
+			require("litee.calltree").setup(opts)
+		end,
+		keys = {
+
+			{
+				"gH",
+				function()
+					vim.lsp.buf.incoming_calls()
+				end,
+				desc = "incoming calls",
+			},
+			{
+				"gl",
+				function()
+					vim.lsp.buf.outgoing_calls()
+				end,
+				desc = "outgoing calls",
+			},
+			{
+				"gj",
+				function()
+					require("litee.calltree").expand_calltree()
+				end,
+				desc = "Expand calltree",
+			},
+		},
+	},
+}
